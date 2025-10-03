8b7d579bbcd984d328ed963bb2da7445da602403
auliazahwaa007 <auliazahwaa007@gmail.com>
2025-10-03 20:51:45 +0700

    memasukkan kode baris ke 3

--- Test.java
+++ Test.java
@@ -4,5 +4,6 @@ public class Test {
         System.out.println("Hai, ini baris kode pertama");
         System.out.println("Halo, ini baris kode kedua");
         System.out.println("Hallo, ini baris kode ketiga");
+        
     }
 }
