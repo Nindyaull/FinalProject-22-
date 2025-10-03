c1401de89d5c2477602efcc6d90fce67e30644c4
auliazahwaa007 <auliazahwaa007@gmail.com>
2025-10-03 20:35:34 +0700

    user 3 menambahkan kode ketiga

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hai, ini baris kode pertama");
         System.out.println("Halo, ini baris kode kedua");
+        System.out.println("Hallo, ini baris kode ketiga");
     }
 }
